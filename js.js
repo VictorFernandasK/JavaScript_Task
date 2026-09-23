c2c68a27cf638a9901086349a8a8f05ce7cb681d
victor <victor123@gmail.com>
2026-09-23 14:51:52 +0530

    initial commit

---
+++ js.js
@@ -0,0 +1,282 @@
+// 1  (Var)
+
+var name = "Victor Fernandas K"
+console.log(name);
+
+// 2
+
+var age = "29"
+console.log(age);
+
+// 3
+
+var a;
+a = 10;
+console.log(a);
+
+// 4
+
+var b = 20
+b = 40
+console.log(b)
+
+// 5
+
+var mobile = "oppo"
+var mobile = "Iphone"
+console.log(mobile);
+
+// 6
+
+var employeename = "Victor Fernandas K"
+var employeeage = 29
+var employeecity = "Thanjavur"
+console.log(employeename);
+console.log(employeeage);
+console.log(employeecity);
+
+// 7
+
+var college = "Hindusthan Institute of Technology"
+console.log(college);
+
+// 8
+
+var favoritesubject = "Tamil History"
+console.log(favoritesubject);
+
+// 9
+
+var number = 100
+number = 200
+number = 300
+number = 400
+console.log(number);
+
+// 10
+
+var salary = "8,000"
+salary = "40,000"
+console.log(salary);
+
+// 11 (Let)
+
+let myname = "Victor Fernandas K"
+console.log(myname);
+
+// 12
+
+let myage = 29
+console.log(myage);
+
+//13
+
+let tv
+tv = "Samsung"
+console.log(tv)
+
+// 14
+
+let value = 100
+value = 200
+console.log(value);
+
+// 15
+
+let Myname = "victor Fernandas"
+let qulification = "B.E - CSE"
+let location1 = "Thanjavur"
+
+console.log(Myname);
+console.log(qulification);
+console.log(location1);
+
+// 16
+
+let course = "JavaScript"
+
+// 17
+
+let mark = 50
+mark = 80
+console.log(mark);
+
+// 18
+
+let companyName = "Stackly"
+console.log(companyName);
+
+// 19
+
+let experience = 1
+
+// 20
+
+let mobileModel = "Iphone"
+console.log(mobileModel);
+
+// 21 (Const)
+
+const Name = "Victor Fernandas K"
+console.log(Name);
+
+// 22
+
+const DOB = "02 March 1997"
+console.log(DOB);
+
+// 23
+
+const company = "Stackly"
+
+// 24
+
+const country = "India"
+
+// 25
+
+const name1 = "Victor Fernandas K"
+const age1 = 29
+const city1 = "thanjavur"
+
+console.log(name1);
+console.log(age1);
+console.log(city1);
+
+// 26
+
+const coursName = "JavaScript"
+
+console.log(coursName);
+
+// 27
+
+const collegeName = "Hindusthan Institute of Technology"
+console.log(collegeName);
+
+// 28
+
+const favoriteColor = "Red and Block"
+console.log(favoriteColor);
+
+// 29
+
+const employeeId = "STK-26-6134"
+console.log(employeeId);
+
+// 30
+
+const officeLocation = "Salem"
+console.log(officeLocation);
+
+// 31 (Prininting Statement)
+
+console.log(100);
+
+// 32 
+
+console.log("Victor Fernandas K")
+
+// 33
+
+console.log(29);
+console.log("B.E - CSE")
+
+// 34
+
+console.log("Welcome to JavaScript");
+
+// 35
+
+console.log("Python")
+console.log(12345)
+console.log("Python 3.14.7")
+console.log("@#$%")
+console.log("victor@123")
+
+// 36
+
+var userName = "Victor"
+console.log(userName)
+
+// 37
+
+var userAge = 29
+console.log(userAge);
+
+// 38
+
+console.log("Victor");
+console.log(29);
+console.log("Thanjavur");
+console.log("B.E - CSE")
+
+// 39 (Popup)
+
+alert("Welcome to JavaScript")
+
+// 40
+
+var name2 = "Victor Fernandas K"
+alert(name2)
+
+// 41
+
+var name3 = prompt("What is your name?")
+console.log(name3);
+
+// 42
+
+var age2 = prompt("Enter your age?")
+console.log(age2);
+
+// 43
+
+let qualification = prompt("Your Qualification")
+console.log(qualification);
+
+// 44
+
+var city2 = prompt("What is your City?")
+console.log(city2);
+
+// 45
+
+confirm("Do you know JavaScript?")
+
+// 46
+
+confirm("Do you want to continue?");
+
+// 47
+
+var Name1 = prompt("What is your name?");
+var qualification1 = prompt("What is your qualification?");
+
+document.writeln(Name1);
+document.writeln(qualification1);
+
+// 48 (Document.writeIn + Console Methods)
+
+var message = "Hello Everyone"
+document.writeln(message);
+
+// 49
+
+console.log("Victor Fernandas K");
+console.warn("B.E - CSE");
+console.error("Thanjavur");
+
+// 50
+
+var Name3 = prompt("What is your name?")
+var age3 = prompt("What is your age?")
+var qualification3 = prompt("What is your qualification?")
+
+console.log(Name3);
+console.warn(age3);
+console.error(qualification3);
+
+document.write(Name3);
+document.writeln(age3);
+document.writeln(qualification3)
